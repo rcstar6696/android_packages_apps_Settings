9e8da8a733cc76cb3584d3fda3fd815be5a7b338
Max Weffers <rcstar6696@gmail.com>
2020-03-10 13:36:16 +0100

    fix pattern
    Change-Id: I780662f4b9fe5445d4fe74483864a5c26ac64f45

--- src/com/android/settings/password/ChooseLockPatternSize.java
+++ src/com/android/settings/password/ChooseLockPatternSize.java
@@ -151,7 +151,7 @@ public class ChooseLockPatternSize extends SettingsActivity {
 
         @Override
         public int getMetricsCategory() {
-            return MetricsEvent.CUSTOM_SETTINGS;
+            return MetricsEvent.CARBONFIBERS;
         }
     }
 }
